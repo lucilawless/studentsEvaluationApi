05fabbb2d99bc6a8013faff790cdf3e988f6e8e1
Luci-Lawless <lu.gmmm@gmail.com>
2017-12-13 22:10:52 +0100

    Add seeds for batches

--- routes/batches.js
+++ routes/batches.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
-const { batch } = require('../models')
+const { Batch } = require('../models')
 const passport = require('../config/auth')
 
 router.get('/batches', (req, res, next) => {
-  batch.find()
+  Batch.find()
     // Newest batches first
     .sort({ startedAt: -1 })
     // Send the data in JSON format
@@ -13,7 +13,7 @@ router.get('/batches', (req, res, next) => {
   })
   .get('/batches/:id', (req, res, next) => {
     const id = req.params.id
-    batch.findById(id)
+    Batch.findById(id)
       .then((batch) => {
         if (!batch) { return next() }
         res.json(batch)
@@ -24,7 +24,7 @@ router.get('/batches', (req, res, next) => {
     let newbatch = req.body
     newbatch.authorId = req.account._id
 
-    batch.create(newbatch)
+    Batch.create(newbatch)
       .then((batch) => res.json(batch))
       .catch((error) => next(error))
   })
